510cc978d5548b8a5690681a3bb833849ce412e6
Satyam Singh <satyamrajput7239@gmail.com>
2025-06-25 11:07:26 +0530

    feat : add page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,24 @@ import {
   Navigate,
 } from "react-router-dom";
 import DashboardHome from "./pages/DashBoardHome";
-
-import Dashboard from "./pages/DashBoard.jsx";
+import Purchases from "./pages/Purchases";
+import Transfers from "./pages/Transfers";
+import Assignments from "./pages/Assignments";
+import Settings from "./pages/Settings";
+import Dashboard from "./pages/DashBoard";
 export default function App() {
   return (
     <Router>
-      <Route path="/" element={<Dashboard />}>
-        <Route index element={<DashboardHome />} />
-      </Route>
+      <Routes>
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<DashboardHome />} />
+          <Route path="purchases" element={<Purchases />} />
+          <Route path="transfers" element={<Transfers />} />
+          <Route path="assignments" element={<Assignments />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </Router>
   );
 }
